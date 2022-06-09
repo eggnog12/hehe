d021945ea30ebbdf93f311584bc7cdb73d9bc5fc
eggnog12 <81947659+eggnog12@users.noreply.github.com>
2022-06-09 12:17:44 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,5 +18,8 @@ basic.forever(function () {
     music.playMelody("E F G F D F E G ", 120)
 })
 basic.forever(function () {
+    music.playMelody("A D F G A D A E ", 120)
+})
+basic.forever(function () {
     music.playMelody("F E E D A G E E ", 120)
 })
